9fa42bda3375c9a005317f74b471acc1190a77d8
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-12-03 13:55:40 +0100

    Implement Router class skeleton with methods for route management

---
+++ framework/router.js
@@ -0,0 +1,13 @@
+export class Router {
+    constructor() {
+        // TODO: initialize routes
+    }
+
+    addRoute(path, callback) {
+        // TODO: register route
+    }
+
+    start() {
+        // TODO: start listening to route changes
+    }
+}
