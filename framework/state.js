ab69f98ac8590e074d28e9423d1d3cb7efda8852
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-12-03 13:56:30 +0100

    Implement Store class skeleton with methods for state management

---
+++ framework/state.js
@@ -0,0 +1,17 @@
+export class Store {
+    constructor(initialState = {}) {
+        // TODO: initialize state
+    }
+
+    getState() {
+        // TODO: return state
+    }
+
+    setState(updater) {
+        // TODO: update state and notify subscribers
+    }
+
+    subscribe(listener) {
+        // TODO: subscribe to changes
+    }
+}
