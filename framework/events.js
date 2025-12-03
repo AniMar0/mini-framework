142ea98ebe0ebda14090e3c4c25b397856030394
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-12-03 13:56:43 +0100

    Implement Events class skeleton with methods for event handling

---
+++ framework/events.js
@@ -0,0 +1,13 @@
+export class Events {
+    constructor() {
+        // TODO: init event map
+    }
+
+    on(eventName, handler) {
+        // TODO: register event
+    }
+
+    trigger(eventName, payload) {
+        // TODO: trigger event
+    }
+}
