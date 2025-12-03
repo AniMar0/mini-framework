95232e6bad60d922904381ee7fdd420536c8ed0a
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-12-03 13:57:12 +0100

    Add framework API exports for core modules

---
+++ framework/index.js
@@ -0,0 +1,12 @@
+import { createElement } from "./dom.js";
+import { Router } from "./router.js";
+import { Store } from "./state.js";
+import { Events } from "./events.js";
+
+// Export framework API
+export default {
+    createElement,
+    Router,
+    Store,
+    Events
+};
